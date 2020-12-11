2b6fd71d3adba16c8aebf849af4611493778b8ca
Will Fleming <wfleming77@gmail.com>
2020-12-10 20:03:53 -0500

    day3 part 2

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -4,26 +4,35 @@ use std::io::Error;
 fn main() -> Result<(), Error> {
     let input = parse_input()?;
 
-    // for c in &password_infos {
-    //     for x in c {
-    //         print!("{}", x);
-    //     }
-    //     println!("")
-    // }
-
     part_one(&input);
+    part_two(&input);
 
     Ok(())
 }
 
 fn part_one(input: &Vec<Vec<char>>) {
+    let result = num_trees_encountered(input, 3, 1);
+    println!("{}", result);
+}
+
+fn part_two(input: &Vec<Vec<char>>) {
+    let values: Vec<(usize, usize)> = vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+
+    let result: usize = values
+        .iter()
+        .map(|(x, y)| num_trees_encountered(input, *x as usize, *y as usize))
+        .product();
+    println!("{}", result);
+}
+
+fn num_trees_encountered(input: &Vec<Vec<char>>, y_step: usize, x_step: usize) -> usize {
     let mut x = 0;
     let mut y = 0;
     let mut acc = 0;
 
     loop {
-        x += 1;
-        y += 3;
+        x += x_step;
+        y += y_step;
         if x >= input.len() {
             break;
         }
@@ -31,7 +40,7 @@ fn part_one(input: &Vec<Vec<char>>) {
             acc += 1;
         }
     }
-    println!("{}", acc);
+    acc
 }
 
 fn parse_input() -> Result<Vec<Vec<char>>, Error> {
